9c9fd4fc54f211ddeb666d9d71920ca4f8db9a4d
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-22 12:29:36 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
+from glob import glob
 
 st.set_page_config(layout='wide')
 
 st.title('Weather Forecasdt for the Next Days')
 
-place = st.text_input('Place:', key='text')
-days = st.slider('Forecast Days', min_value=1, max_value=5, help='Select the number of days', key='value')
+place: str = st.text_input('Place:', key='text', help='Escreve uma cidade válida')
+days: str = st.slider('Forecast Days', min_value=1, max_value=7, help='Select the number of days', key='value')
 
-options = st.selectbox('Select data to view', ['Temperature', 'Sky'], key='box')
+options: str = st.selectbox('Select data to view', ['Temperature', 'Sky'], key='box')
 
-if place:
-    st.subheader(f'{options} for the next {days} days in {place}')
-    figure = px.line(x=['a', 'b', 'c'], y=[9, 10, 12], labels={'x': 'Date', 'y': 'Temperature(C)'})
-    st.plotly_chart(figure)
+try:
+    if place:
+        st.subheader(f'{options} for the next {days} days in {place}')
+        data_list: list = get_data(place, days, options)
+        print(data_list)
+        if options == 'Temperature':
+            figure = px.line(x=data_list[1], y=data_list[0], labels={'x': 'Date', 'y': 'Temperature(C)'})
+            st.plotly_chart(figure)
+        else:
+            images_list: list = glob('*.png')
+            image_path = [f'{sky.casefold()}.png' for sky in data_list[0]]
+            st.image(image_path, width=400)
+except TypeError:
+    st.write('Por favor, escreve uma cidade válida!')
